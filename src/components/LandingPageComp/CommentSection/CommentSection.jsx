ddaccb043f22d5a743e6aeb8457a8fc9a4735f5b
Dmitriy-Ponomarenko <dponomar19@gmail.com>
2025-07-29 13:21:33 +0200

    comments structure

--- src/components/LandingPageComp/CommentSection/CommentSection.jsx
+++ src/components/LandingPageComp/CommentSection/CommentSection.jsx
@@ -3,13 +3,21 @@
 import React from 'react';
 import styles from './CommentSection.module.css';
 import '../../../index.css';
+import { Navigate } from 'react-router-dom';
 
 export const CommentSection = () => {
   return (
     <section className={`${styles.commentSection} section`}>
       <div className={`${styles.commentContainer} container`}>
-        <h2>Comments</h2>
-        {/* Comment form and list goes here */}
+        <p>What people think?</p>
+        <h2>Press quotes</h2>
+        <p>
+          Our goal is to create a product and service that you’re satisfied with and use it every day. This is why we’re
+          constantly working on our services to make it better every day and really listen to what our users has to say.
+        </p>
+        <Link to="/comments" className={styles.commentButton}>
+          Read more testimonials
+        </Link>
       </div>
     </section>
   );
