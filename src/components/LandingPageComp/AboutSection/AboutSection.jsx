080e04781dbebf0138c138f8a7cb532af3e5de9d
Dmitriy-Ponomarenko <dponomar19@gmail.com>
2025-07-30 16:08:17 +0200

    hotfix pictures

--- src/components/LandingPageComp/AboutSection/AboutSection.jsx
+++ src/components/LandingPageComp/AboutSection/AboutSection.jsx
@@ -11,34 +11,52 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
+import About1 from '../../../assets/aboutSection/pc/about1.jpg';
+import About1x2 from '../../../assets/aboutSection/pc/about1@2x.jpg';
+import About2 from '../../../assets/aboutSection/pc/about2.jpg';
+import About2x2 from '../../../assets/aboutSection/pc/about2@2x.jpg';
+import About3 from '../../../assets/aboutSection/pc/about3.jpg';
+import About3x2 from '../../../assets/aboutSection/pc/about3@2x.jpg';
+import About4 from '../../../assets/aboutSection/pc/about4.jpg';
+import About4x2 from '../../../assets/aboutSection/pc/about4@2x.jpg';
+
+import AboutMobile1 from '../../../assets/aboutSection/mobile/about-mobile1.jpg';
+import AboutMobile1x2 from '../../../assets/aboutSection/mobile/about-mobile1@2x.jpg';
+import AboutMobile2 from '../../../assets/aboutSection/mobile/about-mobile2.jpg';
+import AboutMobile2x2 from '../../../assets/aboutSection/mobile/about-mobile2@2x.jpg';
+import AboutMobile3 from '../../../assets/aboutSection/mobile/about-mobile3.jpg';
+import AboutMobile3x2 from '../../../assets/aboutSection/mobile/about-mobile3@2x.jpg';
+import AboutMobile4 from '../../../assets/aboutSection/mobile/about-mobile4.jpg';
+import AboutMobile4x2 from '../../../assets/aboutSection/mobile/about-mobile4@2x.jpg';
+
 const slides = [
   {
     alt: 'Island overview',
-    desktop: '/src/assets/aboutSection/pc/about1.jpg',
-    desktop2x: '/src/assets/aboutSection/pc/about1@2x.jpg',
-    mobile: '/src/assets/aboutSection/mobile/about-mobile1.jpg',
-    mobile2x: '/src/assets/aboutSection/mobile/about-mobile1@2x.jpg',
+    desktop: About1,
+    desktop2x: About1x2,
+    mobile: AboutMobile1,
+    mobile2x: AboutMobile1x2,
   },
   {
     alt: 'Monsters encounter',
-    desktop: '/src/assets/aboutSection/pc/about2.jpg',
-    desktop2x: '/src/assets/aboutSection/pc/about2@2x.jpg',
-    mobile: '/src/assets/aboutSection/mobile/about-mobile2.jpg',
-    mobile2x: '/src/assets/aboutSection/mobile/about-mobile2@2x.jpg',
+    desktop: About2,
+    desktop2x: About2x2,
+    mobile: AboutMobile2,
+    mobile2x: AboutMobile2x2,
   },
   {
     alt: 'Contestants escaping',
-    desktop: '/src/assets/aboutSection/pc/about3.jpg',
-    desktop2x: '/src/assets/aboutSection/pc/about3@2x.jpg',
-    mobile: '/src/assets/aboutSection/mobile/about-mobile3.jpg',
-    mobile2x: '/src/assets/aboutSection/mobile/about-mobile3@2x.jpg',
+    desktop: About3,
+    desktop2x: About3x2,
+    mobile: AboutMobile3,
+    mobile2x: AboutMobile3x2,
   },
   {
     alt: 'Final escape',
-    desktop: '/src/assets/aboutSection/pc/about4.jpg',
-    desktop2x: '/src/assets/aboutSection/pc/about4@2x.jpg',
-    mobile: '/src/assets/aboutSection/mobile/about-mobile4.jpg',
-    mobile2x: '/src/assets/aboutSection/mobile/about-mobile4@2x.jpg',
+    desktop: About4,
+    desktop2x: About4x2,
+    mobile: AboutMobile4,
+    mobile2x: AboutMobile4x2,
   },
 ];
 
