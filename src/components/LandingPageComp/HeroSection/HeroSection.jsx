92d8d112463027b723c084b39960558c0f17b515
Maksym Yaroshenko <yiaroshenko.max@gmail.com>
2025-08-06 01:19:37 +0300

    addda

--- src/components/LandingPageComp/HeroSection/HeroSection.jsx
+++ src/components/LandingPageComp/HeroSection/HeroSection.jsx
@@ -8,7 +8,7 @@ export const HeroSection = () => {
   return (
     <section className={`${styles.heroSection} section`}>
       <div className={`${styles.heroContainer} container`}>
-        <h1>SURVIVE AT ALL COSTS</h1>
+        <h1>SURVIVE AT ALL COSTS auf</h1>
         <p>Experience new social battle royale game</p>
       </div>
     </section>
